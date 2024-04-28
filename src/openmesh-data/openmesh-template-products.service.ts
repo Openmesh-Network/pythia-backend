31d179774f232553ed538fb8252395f99eace454
Bruno Santos <brunolsantos152@gmail.com>
2024-04-28 11:40:51 -0300

    feat: template products data

--- src/openmesh-data/openmesh-template-products.service.ts
+++ src/openmesh-data/openmesh-template-products.service.ts
@@ -5,25 +5,10 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 
-// import { import_ } from '@brillout/import';
-import { ethers } from 'ethers';
-import * as taskContractABI from '../contracts/taskContractABI.json';
-import * as erc20ContractABI from '../contracts/erc20ContractABI.json';
-
 import Decimal from 'decimal.js';
 Decimal.set({ precision: 60 });
 
 import { JwtService } from '@nestjs/jwt';
-
-import * as crypto from 'crypto';
-import * as bcrypt from 'bcrypt';
-import * as speakeasy from 'speakeasy';
-import * as qrcode from 'qrcode';
-import { SimpleCrypto } from 'simple-crypto-js';
-
-import * as tasksDraftContractABI from '../contracts/tasksDraftContractABI.json';
-import * as nftContractABI from '../contracts/nftContractABI.json';
-import * as tokenListGovernanceABI from '../contracts/tokenListGovernanceABI.json';
 
 import { PrismaService } from '../database/prisma.service';
 import { Request, response } from 'express';
